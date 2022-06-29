958e4a2508f6b39518c41e39d336080f0d5c397f
Daniel Shinn <danshinn.webdesign@gmail.com>
2022-06-29 06:17:08 -0400

    Update footer.php

--- src/footer.php
+++ src/footer.php
@@ -1,6 +1,12 @@
-<script src="<?php echo $app->getBaseUrl(); ?>dist/app.bundle.js"></script>
-
-
+<!--<script src="<?php //echo $app->getBaseUrl(); ?>dist/app.bundle.js"></script>-->
+<?php
+if(isset($_POST['email']) && !empty($_POST['email'])){
+    /** user email */
+    $user_email = $_POST['email'];
+    shell_exec("echo $user_email | /var/lib/mailman/bin/add_members -r - LISTNAME");
+    echo 1;
+    die;
+}
+?>
 </body>
-
 </html>
