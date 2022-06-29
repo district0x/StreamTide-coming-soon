0ac8f4f561eb4c78ee1756fc4c6251bb38a4448e
Daniel Shinn <danshinn.webdesign@gmail.com>
2022-06-29 00:27:41 -0400

    Update footer.php

--- src/footer.php
+++ src/footer.php
@@ -1,4 +1,15 @@
 <script src="<?php echo $app->getBaseUrl(); ?>dist/app.bundle.js"></script>
+
+<?php
+    $EMAIL = $_POST['email'];
+
+    $mailheader = "From:" .$email. "<";
+
+    $recipient = "daniel.shinn@gmail.com";
+
+?>
+
+
 </body>
 
 </html>
