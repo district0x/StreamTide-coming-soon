3a6c4227d73096469490f3efe0180ff7c15426a5
Daniel Shinn <danshinn.webdesign@gmail.com>
2022-07-04 15:21:46 -0400

    Update home.php

--- src/paginas/home/home.php
+++ src/paginas/home/home.php
@@ -67,7 +67,7 @@
 	<section class="l-page__content">
 		<div class="l-page__content__wrap">
 			<div class="l-page__content__description">
-				<h1>COMNING SOON!</h1>
+				<h1>COMING SOON!</h1>
 				<p>Stream Tide is on a mission to solve “Starving Artist Syndrome” by turning a small stream of financial support into a tidal wave of support!
 				 You support the creators you love and the community will match those donations from our grant pool. In exchange, you get exclusive access and content from the creators you love. Support what you love, and so will our community!</p>
 			</div>
