f4e760e24e6a0ee1da4e4a341fa9147fdfbfdaaf
Daniel Shinn <danshinn.webdesign@gmail.com>
2022-07-10 19:39:15 -0400

    Update menu-mobile.php

--- src/partials/shared/menu-mobile.php
+++ src/partials/shared/menu-mobile.php
@@ -12,9 +12,9 @@
 						<img class="i-day" src="<?php echo $this->getBaseUrl(); ?>img/layout/day.svg" alt="">
 					</span>
 				</div>
-				<li><a class="c-menu-mobile__nav__item" href="#">Placeholder Link</a></li>
-				<li><a class="c-menu-mobile__nav__item" href="#">Placeholder Link</a></li>
-				<li><a class="c-menu-mobile__nav__item" href="#">Placeholder Link</a></li>
+				<li><a target="_blank" href="https://twitter.com/StreamTide" class="c-menu-mobile__nav__item">Twitter</a></li>
+				<li><a target="_blank" href="https://mirror.xyz/streamtide.eth" class="c-menu-mobile__nav__item">Blog</a></li>
+				<li><a target="_blank" href="https://discord.gg/RUmWpVneeV" class="c-menu-mobile__nav__item">Discord</a></li>
 			</ul>
 		</div>
 	</div>
